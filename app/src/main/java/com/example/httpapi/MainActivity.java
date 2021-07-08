7b119181838be981942ed83eb8494466dac3d1a2
Satyam Vishwakarma <2018.satyam.vishwakarma@ves.ac.in>
2021-07-08 18:36:21 +0530

    GET working

---
+++ app/src/main/java/com/example/httpapi/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.httpapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class MainActivity extends AppCompatActivity {
+    String BASE_URL = "http://192.168.43.237:5000/api/v1/example/";
+    EditText nameEditText;
+    TextView msgTxt;
+    boolean sqlClicked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nameEditText = (EditText) findViewById(R.id.editText);
+        msgTxt = findViewById(R.id.textView);
+        Button getBtn = (Button) findViewById(R.id.get);
+        Button postBtn = (Button) findViewById(R.id.post);
+        Button sqlBtn = (Button) findViewById(R.id.sql);
+        sqlClicked = false;
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "Thread running", Toast.LENGTH_SHORT).show();
+                //Your code goes here
+                String name = getTextFromEditText(nameEditText);
+                String link = BASE_URL + "get?name=" + name;
+                sqlClicked = false;
+                Task task = new Task();
+                task.execute(link);
+            }
+        });
+
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "POST request", Toast.LENGTH_SHORT).show();
+                //Your code goes here
+                String name = getTextFromEditText(nameEditText);
+                String link = BASE_URL + "post";
+                sqlClicked = false;
+                Task2 task2 = new Task2();
+                task2.execute(link, name);
+            }
+        });
+
+        sqlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "Thread running", Toast.LENGTH_SHORT).show();
+                //Your code goes here
+                String name = getTextFromEditText(nameEditText);
+                String link = BASE_URL + "sql";
+                sqlClicked = true;
+                Task task = new Task();
+                task.execute(link);
+            }
+        });
+    }
+
+    public String contactAPI(String link) {
+        String jsonResponse = "";
+        URL url = QueryUtils.createUrl(link);
+        try {
+            return jsonResponse = QueryUtils.makeHttpRequest(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonResponse;
+    }
+
+    public String postToAPI(String name, String link) {
+        String jsonResponse = "";
+        URL url = QueryUtils.createUrl(link);
+        return jsonResponse = QueryUtils.makeHttpPostRequest(url, name);
+    }
+
+    public String getTextFromEditText(EditText editText) {
+        return editText.getText().toString();
+    }
+
+    private class Task extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("MainActivity.this", "Bg= " + strings[0]);
+            return contactAPI(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            String msg = "";
+            ArrayList<String> namesArray = new ArrayList<>();
+            if (sqlClicked) {
+                namesArray = QueryUtils.extractJsonFromUserDb(s);
+                sqlClicked = false;
+
+                StringBuilder listString = new StringBuilder();
+
+                for (String str : namesArray) {
+                    listString.append(str).append(" ");
+                }
+                msgTxt.setText("All names= " + listString.toString());
+
+            } else {
+                msg = QueryUtils.extractJson(s);
+                msgTxt.setText("msg= " + msg);
+                Toast.makeText(MainActivity.this,
+                        "GET request= " + s, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class Task2 extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("MainActivity.this", "Bg= " + strings[0]);
+            return postToAPI(strings[0], strings[1]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            String msg = QueryUtils.extractJson(s);
+            msgTxt.setText("msg= " + msg);
+            Toast.makeText(MainActivity.this,
+                    "POST request= " + s, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/httpapi/QueryUtils.java
@@ -0,0 +1,185 @@
+package com.example.httpapi;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class QueryUtils {
+    //Tag for the log messages
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    private QueryUtils() {}
+
+    public static ArrayList<String> extractJsonFromUserDb(String jsonResponse) {
+        Log.v(LOG_TAG, "Extracting JSON from User DB");
+        ArrayList<String> nameArray=new ArrayList<>();
+        try {
+            JSONObject root = new JSONObject(jsonResponse);
+            JSONArray itemsArray=root.getJSONArray("data");
+//            JSONArray itemsArray = root.getJSONArray("msg");
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject iItem = itemsArray.getJSONObject(i);//Accessing i'th element of JSONArray items
+                String message = iItem.getString("fname");
+                nameArray.add(message);
+//                JSONObject msgJSON = iItem.getJSONObject("volumeInfo");
+//                String message = msgJSON.getString("title");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return nameArray;
+    }
+
+    //parsing json response
+    public static String extractJson(String jsonResponse) {
+        Log.v(LOG_TAG, "Extracting JSON");
+        String msg="";
+        try {
+            JSONObject root = new JSONObject(jsonResponse);
+            msg=root.getString("msg");
+//            JSONArray itemsArray = root.getJSONArray("msg");
+//            for (int i = 0; i < itemsArray.length(); i++) {
+//                JSONObject iItem = itemsArray.getJSONObject(i);//Accessing i'th element of JSONArray items
+//                JSONObject msgJSON = iItem.getJSONObject("volumeInfo");
+//                String message = msgJSON.getString("title");
+//            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return msg;
+    }
+
+    // Make an HTTP request to the given URL and return a String as the response.
+    static String makeHttpRequest(URL url) throws IOException {
+        Log.v(LOG_TAG, "I'm making HTTP Request");
+
+        String jsonResponse = "";
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();//open connection at url
+            urlConnection.setRequestMethod("GET");//set the request method-GET,PUT or DELETE
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();//connect to the server
+            int responseCode = urlConnection.getResponseCode();//store the received response code from the server
+            //if connection is successful then response code is 200
+            if (responseCode == 200) {
+                inputStream = urlConnection.getInputStream();//server sends data as InputStream
+                jsonResponse = readFromStream(inputStream);//convert received InputStream to String jsonResponse
+            } else {
+                Log.e(LOG_TAG, "Status Code is " + responseCode);
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "IOException while making HTTP request ", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    static String makeHttpPostRequest(URL url,String data){
+        String jsonResponse = "";
+        if (url == null) {
+            return jsonResponse;
+        }
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try{
+            urlConnection = (HttpURLConnection) url.openConnection();//open connection at url
+            urlConnection.setRequestMethod("POST");//set the request method-GET,PUT or DELETE
+            urlConnection.setRequestProperty("Content-Type","application/json; utf-8");
+            urlConnection.setRequestProperty("Accept","application/json");
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+
+            String jsonInput=getJSONdata(data);
+            try(OutputStream os = urlConnection.getOutputStream()){
+               byte[] input=jsonInput.getBytes("utf-8");
+               os.write(input,0,input.length);
+            }
+
+            urlConnection.connect();//connect to the server
+
+            int responseCode = urlConnection.getResponseCode();//store the received response code from the server
+            //if connection is successful then response code is 200
+            if (responseCode == 200) {
+                inputStream = urlConnection.getInputStream();//server sends data as InputStream
+                jsonResponse = readFromStream(inputStream);//convert received InputStream to String jsonResponse
+            } else {
+                Log.e(LOG_TAG, "Status Code is " + responseCode);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return jsonResponse;
+    }
+
+    static URL createUrl(String stringUrl) {
+        Log.v(LOG_TAG, "I'm creating URL");
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        Log.v(LOG_TAG, "I'm in reading from stream");
+        StringBuilder stringBuilder = new StringBuilder();//Better than String
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();//reading the first line of the inputStream
+            while (line != null) {
+                stringBuilder.append(line);//adding the line(of the inputStream to the StringBuilder)
+                line = reader.readLine();//reading the next line of inputStream
+            }
+        }
+        return stringBuilder.toString();//converting StringBuilder output to String(The jsonResponse code)
+    }
+
+    static String getJSONdata(String data){
+        return "{"+"\"name:\""+"\""+data+"\""+"}";
+    }
+
+}
+
